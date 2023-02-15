eadfda8ba041072825acf51d1b191d0015c79455
JulianAG420 <101308341+JulianAG420@users.noreply.github.com>
2023-02-14 19:30:59 -0600

    commit 1

---
+++ ProyectoCafe4/src/test/java/PCafe/grupo4/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package PCafe.grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
