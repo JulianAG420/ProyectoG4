4a94f01eae396f9770ccaa190eb6dae46a954398
Roberto2401 <robertoiribarren020@gmail.com>
2023-03-13 22:30:52 -0600

    commit proyecto
    arreglo de los errores previos

---
+++ ProyectoGrupo4Oficial/src/main/java/ProyectoG4/ProyectoG4/ProyectoG4Application.java
@@ -0,0 +1,13 @@
+package ProyectoG4.ProyectoG4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoG4Application.class, args);
+	}
+
+}
